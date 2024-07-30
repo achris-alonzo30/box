2e9882ee7cbde2194477f45d435079ca2debc50a
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-30 15:46:34 -0300

    wrap the DashboardWrapper with StoreProvider

--- src/components/dashboard-wrapper.tsx
+++ src/components/dashboard-wrapper.tsx
@@ -1,6 +1,29 @@
-import { cn } from "@/lib/utils"
-import { Navbar } from "./navbar"
-import { Sidebar } from "./sidebar"
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Navbar } from "./navbar";
+import { Sidebar } from "./sidebar";
+import StoreProvider, { useAppSelector } from "@/store/redux";
+
+const DashboardLayout = ({
+    children
+}: {
+    children: React.ReactNode
+}) => {
+    const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+    
+    return (
+        <main className={cn("flex w-full min-h-screen")}>
+            <Sidebar />
+            <section className={cn("flex flex-col w-full h-full py-7 px-9",
+                isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+            )}>
+                <Navbar />
+                {children}
+            </section>
+        </main>
+    )
+}
 
 export const DashboardWrapper = ({
     children
@@ -8,12 +31,8 @@ export const DashboardWrapper = ({
     children: React.ReactNode
 }) => {
     return (
-        <main className={cn("flex w-full min-h-screen")}>
-            <Sidebar />
-            <section className={cn("flex flex-col w-full h-full py-7 px-9 md:pl-24 ")}>
-                <Navbar />
-                {children}
-            </section>
-        </main>
+        <StoreProvider>
+            <DashboardLayout>{children}</DashboardLayout>
+        </StoreProvider>
     )
 }
