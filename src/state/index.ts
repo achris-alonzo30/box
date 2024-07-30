6e06645b1e488942aa2213fb989f13c79bce0aca
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-30 15:19:25 -0300

    create global slice for the sidebar collapse state

---
+++ src/state/index.ts
@@ -0,0 +1,22 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const initialState: { isSidebarCollapsed: boolean} = {
+    isSidebarCollapsed: false
+}
+
+// reducers.state takes in the keys of the initial state
+// reducers.actions takes in the upcoming values received from the reducer
+
+export const globalSlice = createSlice({
+    name: "global",
+    initialState,
+    reducers: {
+        setIsSideBarCollapsed: (state, action: PayloadAction<boolean>) => {
+            state.isSidebarCollapsed = action.payload;
+        },
+    }
+});
+
+export const { setIsSideBarCollapsed } = globalSlice.actions;
+
+export default globalSlice.reducer
