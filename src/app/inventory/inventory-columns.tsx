a2813643b792b22a1d63ed6dec6bce37874ab84f
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-31 17:24:20 -0300

    add new column called actions

--- src/app/inventory/inventory-columns.tsx
+++ src/app/inventory/inventory-columns.tsx
@@ -1,9 +1,18 @@
 "use client"
 
-import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
+import {
+    DropdownMenu,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+    DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+
 export type Payment = {
     id: string
     amount: number
@@ -48,6 +57,34 @@ export const inventoryColumns: ColumnDef<Payment>[] = [
             }).format(amount)
 
             return <div className="text-right font-medium">{formatted}</div>
+        },
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const payment = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => navigator.clipboard.writeText(payment.id)}
+                        >
+                            Copy payment ID
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>View customer</DropdownMenuItem>
+                        <DropdownMenuItem>View payment details</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
         },
     },
 ]
